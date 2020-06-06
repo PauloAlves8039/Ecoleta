550ad55406aef12c6fd6eb1da9bbf4fc5cb2ea26
PauloAlves8039 <pj38alves@gmail.com>
2020-06-06 16:22:58 -0300

    Alterando propriedades para upload de imagem

--- mobile/src/pages/Detail/index.tsx
+++ mobile/src/pages/Detail/index.tsx
@@ -30,6 +30,7 @@ interface Params {
 interface Data {
   point: {
     image: string;
+    image_url: string;
     name: string;
     email: string;
     whatsapp: string;
@@ -101,7 +102,7 @@ const Detail: React.FC = () => {
 
                 <Image
                     style={styles.pointImage}
-                    source={{ uri: data.point.image }} />
+                    source={{ uri: data.point.image_url }} />
 
                 <Text style={styles.pointName}>{data.point.name}</Text>
                 <Text style={styles.pointItems}>{data.items.map((item) => item.title).join(",")}</Text>
