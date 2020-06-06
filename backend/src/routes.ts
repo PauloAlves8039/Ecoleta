4e76c9fb59580b3a4b6bc184755e6274ed748a77
PauloAlves8039 <pj38alves@gmail.com>
2020-06-06 18:14:00 -0300

    Adicionando validação de campos

--- backend/src/routes.ts
+++ backend/src/routes.ts
@@ -6,6 +6,7 @@
  */
 
 import express from 'express';
+import { celebrate, Joi } from 'celebrate';
 
 import multer from 'multer';
 import multerConfig from './config/multer';
@@ -27,7 +28,25 @@ routes.get('/items', itemsConstroller.index);
 /**
  * @description: rota responsável por inserir pontos de coleta.
  */
-routes.post('/points', upload.single('image'), pointsController.create);
+routes.post(
+    '/points', 
+    upload.single('image'),
+     celebrate({
+         body: Joi.object().keys({
+             name: Joi.string().required(),
+             email: Joi.string().required().email(),
+             whatsapp: Joi.number().required(),
+             latitude: Joi.number().required(),
+             longitude: Joi.number().required(),
+             city: Joi.string().required(),
+             uf: Joi.string().required().max(2),
+             items: Joi.string().required(),
+         })
+     }, {
+         abortEarly: false
+     }),
+    pointsController.create
+    );
 
 /**
  * @description: rota responsável por listar todos os pontos de coleta.
